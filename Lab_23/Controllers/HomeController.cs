c45ad7e3a0617bffa03aac2db310120f8c0f67d5
kman12681 <keith.rigling@gmail.com>
2018-05-30 23:01:41 -0400

    first commit, all extended challenges

---
+++ Lab_23/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Lab_23.Models;
+
+
+namespace Lab_23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CoffeeEntities orm = new CoffeeEntities();
+
+            ViewBag.Items = orm.Items.ToList();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult ItemByName(string byName)
+        {
+            CoffeeEntities orm = new CoffeeEntities();
+            List<Item> itemList = orm.Items.ToList();
+            List<Item> newItems = new List<Item>();
+
+            foreach (Item i in itemList)
+            {
+                if (i.Name == byName)
+                {
+                    newItems.Add(i);
+                }
+            }            
+
+            ViewBag.Item = newItems.ToList();
+
+            return View();
+
+        }
+    }
+}
---
+++ Lab_23/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_23.Models;
+using System.Data.Entity;
+
+namespace Lab_23.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //ActionResult About(Item data) from EF demo today, Users, not items, though.
+        public ActionResult Register(User data)
+        {
+            CoffeeEntities orm = new CoffeeEntities();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    orm.Users.Add(data);
+                    orm.SaveChanges();
+                    ViewBag.Message = $"{data.firstName} has been registered.";
+                }
+                catch (Exception e)
+                {
+
+                    ViewBag.Message = $"Something went wrong: {e.Message}";
+                }
+            }
+            else
+            {
+                ViewBag.Message = $"Invalid entry, nothing added to database.";
+            }
+
+            return View();
+        }
+
+        // old code before connected to database
+
+        //    string firstName = "",
+        //    string lastName = "",
+        //    string email = "",
+        //    int number = 0,
+        //    string password = "",
+        //    int cupsDay = 0,
+        //    string strength = "")
+        //{
+        //    ViewBag.FirstName = firstName;
+        //    ViewBag.LastName = lastName;
+        //    ViewBag.Email = email;
+        //    ViewBag.Number = number;
+        //    ViewBag.Password = password;
+        //    ViewBag.CupsDay = cupsDay;
+        //    ViewBag.Strength = strength;
+        //    return View();
+        //}
+    }
+}
